a23967f57fc784ac14cc0f86e33ee459b3aa44a2
Sharon Watkins <swatkins876@gmail.com>
2019-09-10 18:38:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 0
+ 
+while number < 10 
+  number -= 1
+  puts "#{number} SECOND(S)!"
 end
+ end 
+puts "HAPPY NEW YEAR!"
+
